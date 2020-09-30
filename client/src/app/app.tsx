1d5d861292f124360cc3a14478e6111de88a2413
Przemysław Szelenberger <p.szelenberger@gmail.com>
2020-10-01 00:21:47 +0200

    Add Welcome Page

--- client/src/app/app.tsx
+++ client/src/app/app.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { Header } from './common/components/header/header';
+import { Welcome } from './pages/welcome';
 
 export const App: React.FunctionComponent = () => (
-  <div className="app">
-    <Header />
-  </div>
+  <BrowserRouter>
+    <div className="app">
+      <Header />
+
+      <main className="main">
+        <Route exact path="/">
+          <Welcome />
+        </Route>
+      </main>
+    </div>
+  </BrowserRouter>
 );
---
+++ client/src/app/pages/welcome/welcome.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Welcome = () => (
+  <section className="welcome">
+    <header className="welcome__header">
+      <h2>Kalkulator ceny kursu</h2>
+    </header>
+
+    <div className="welcome__content">
+      <p>Tutaj pójdzie przykładowy tekst powitalny.</p>
+      <button>Przejdź do kalkulatora</button>
+    </div>
+  </section>
+);
