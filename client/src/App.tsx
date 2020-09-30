d65f6bc0bfa5d12dae92d6f33b925446f84ec9cd
Przemysław Szelenberger <p.szelenberger@gmail.com>
2020-09-30 23:09:36 +0200

    Move files into client folder & rid of unneeded files

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// ! Zmień na dostarczone logo
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
