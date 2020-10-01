8f68979c819ad48389070adc98698470a942ca07
Przemysław Szelenberger <p.szelenberger@gmail.com>
2020-10-01 22:12:25 +0200

    Add standard bonuses

--- client/src/app/pages/calculator/calculator.test.tsx
+++ client/src/app/pages/calculator/calculator.test.tsx
@@ -8,3 +8,16 @@ test('renders standard bonuses header', () => {
 
   expect(header).toBeInTheDocument();
 });
+
+test('renders list of standard bonuses', () => {
+  const { getByText } = render(<Calculator />);
+  const bonus1 = getByText(/podręcznik do pobrania/i);
+  const bonus2 = getByText(/ograniczona ilość uczestników/i);
+  const bonus3 = getByText(/wywiady z ekspertami/i);
+  const bonus4 = getByText(/transkrypcje wideo/i);
+
+  expect(bonus1).toBeInTheDocument();
+  expect(bonus2).toBeInTheDocument();
+  expect(bonus3).toBeInTheDocument();
+  expect(bonus4).toBeInTheDocument();
+});
--- client/src/app/pages/calculator/calculator.tsx
+++ client/src/app/pages/calculator/calculator.tsx
@@ -1,10 +1,59 @@
 import React from 'react';
 import { Card, CardContent, Typography } from '@material-ui/core';
+import { Field } from 'formik';
+import { CheckboxWithLabel } from 'formik-material-ui';
+import {
+  FormikStepper,
+  FormikStep,
+} from '../../common/components/formik-stepper';
+
+const standardBonusesData = [
+  {
+    name: 'bonus1',
+    label: 'Podręcznik do pobrania',
+  },
+  {
+    name: 'bonus2',
+    label: 'Ograniczona ilość uczestników',
+  },
+  {
+    name: 'bonus3',
+    label: 'Wywiady z ekspertami',
+  },
+  {
+    name: 'bonus4',
+    label: 'Transkrypcje wideo',
+  },
+];
+
+const renderStandardBonuses = () =>
+  standardBonusesData.map(({ name, label }) => (
+    <Field
+      key={name}
+      name={name}
+      type="checkbox"
+      component={CheckboxWithLabel}
+      Label={{ label }}
+    />
+  ));
 
 export const Calculator: React.FunctionComponent = () => (
   <Card>
     <CardContent>
-      <Typography variant="h2">Standardowe bonusy</Typography>
+      <FormikStepper
+        initialValues={{
+          bonus1: false,
+          bonus2: false,
+          bonus3: false,
+          bonus4: false,
+        }}
+        onSubmit={() => {}}
+      >
+        <FormikStep label="Cena">
+          <Typography variant="h2">Standardowe bonusy</Typography>
+          {renderStandardBonuses()}
+        </FormikStep>
+      </FormikStepper>
     </CardContent>
   </Card>
 );
