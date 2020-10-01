3bc9cb308c238a39b6eb2786086d216616f2d52c
Przemysław Szelenberger <p.szelenberger@gmail.com>
2020-10-01 20:20:54 +0200

    Implement FormikStepper

---
+++ client/src/app/common/components/formik-stepper/formik-stepper.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import {
+  Stepper,
+  Step,
+  StepLabel,
+  Grid,
+  Button,
+  CircularProgress,
+} from '@material-ui/core';
+import { Form } from 'formik/dist/Form';
+import { Formik } from 'formik/dist/Formik';
+import { FormikConfig, FormikValues } from 'formik/dist/types';
+
+const sleep = (time: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, time));
+
+export interface FormikStepProps
+  extends Pick<FormikConfig<FormikValues>, 'children' | 'validationSchema'> {
+  label: string;
+}
+
+export function FormikStep({ children, ...props }: FormikStepProps) {
+  return <React.Fragment>{children}</React.Fragment>;
+}
+
+export function FormikStepper({
+  children,
+  ...props
+}: FormikConfig<FormikValues>) {
+  const childrenArray = React.Children.toArray(children) as React.ReactElement<
+    FormikStepProps
+  >[];
+  const [step, setStep] = useState(0);
+  const [completed, setCompleted] = useState(false);
+  const history = useHistory();
+  const currentChild = childrenArray[step] as React.ReactElement<
+    FormikStepProps
+  >;
+  const isLastStep = () => step === childrenArray.length - 1;
+
+  return (
+    <Formik
+      {...props}
+      validationSchema={currentChild.props.validationSchema}
+      onSubmit={async (values, helpers) => {
+        if (isLastStep()) {
+          await props.onSubmit(values, helpers);
+          setCompleted(true);
+
+          await sleep(1000);
+          history.push('/');
+        } else {
+          setStep((step) => step + 1);
+        }
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form autoComplete="off">
+          <Stepper alternativeLabel activeStep={step}>
+            {childrenArray.map((child, index) => (
+              <Step
+                key={child.props.label}
+                completed={step > index || completed}
+              >
+                <StepLabel>{child.props.label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+
+          {currentChild}
+
+          <Grid container spacing={2}>
+            {step > 0 ? (
+              <Grid item>
+                <Button
+                  disabled={isSubmitting}
+                  variant="contained"
+                  color="primary"
+                  onClick={() => setStep((step) => step - 1)}
+                >
+                  Back
+                </Button>
+              </Grid>
+            ) : null}
+            <Grid item>
+              <Button
+                startIcon={
+                  isSubmitting ? <CircularProgress size="1rem" /> : null
+                }
+                disabled={isSubmitting}
+                variant="contained"
+                color="primary"
+                type="submit"
+              >
+                {isLastStep() ? 'Submit' : 'Next'}
+              </Button>
+            </Grid>
+          </Grid>
+        </Form>
+      )}
+    </Formik>
+  );
+}
